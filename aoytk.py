83883220056c8013acc282d5298c6413b11b0e2b
Tim Ribaric <tim.ribaric@gmail.com>
2023-03-07 14:51:48 -0500

    first work with LDA stuff

--- aoytk.py
+++ aoytk.py
@@ -6,6 +6,9 @@ import requests
 import os
 import pandas as pd
 import matplotlib as plt 
+import re
+from google.colab import drive
+from IPython.display import clear_output
 
 # Global path variable -- a default for Google Drive usage
 path = "/content/drive/MyDrive/AOY/" # default path, can be overwritten by the path-setter widget
@@ -14,15 +17,44 @@ path = "/content/drive/MyDrive/AOY/" # default path, can be overwritten by the p
 def display_path_select(): 
     """Displays a text box to set the default path for reading / writing data
     """
-    txt_path = widgets.Text(description="Folder path:")
+    txt_path = widgets.Text(description="Folder path:", placeholder = "Enter your folder path", value = "/content/drive/MyDrive/AOY/")
     def btn_set_path(btn): 
         global path
-        path = txt_path.value
-        print(f"Folder path set to: {path}")
+        if txt_path.value == "": 
+          print("Please enter a path value.")
+        else: 
+          path = txt_path.value
+          print(f"Folder path set to: {path}")
     btn_txt_submit = widgets.Button(description="Submit")
     btn_txt_submit.on_click(btn_set_path)
     display(txt_path)
     display(btn_txt_submit)
+
+def get_files(main_directory, file_types):
+  """Recursively list files of given types from directory + its subdirectories.
+
+    Args: 
+      main_directory (str): the root directory to look for files in 
+        (including its subdirectories). 
+      file_types (tuple of str): file types to match on ex. (".csv", ".parquet", ".pqt")
+    
+    Returns: 
+      a list of the matching file names, including their path relative to the top level directory
+      Ex. a file in the top-level directory will only return the file name, 
+          a file in a sub directory will include the subdirectory name and 
+          the file name. ex. "subdir/a.csv"
+  """
+  matched_files = []
+  for dirpath, subdirs, files in os.walk(path):
+    subfolder = re.sub(path, "", dirpath)
+    datafiles = [f for f in files if f.endswith(file_types)]
+    for f in datafiles: 
+      if subfolder == "": 
+        matched_files.append(f)
+      else:
+        matched_files.append(f"{subfolder}/{f}")
+  return matched_files
+
 
 # Fletcher's code to download a WARC file from a direct link 
 def download_file(url, filepath='', filename=None, loud=True):
@@ -56,6 +88,7 @@ def download_file(url, filepath='', filename=None, loud=True):
             percent = int((total_bytes_dl / content_len) * 100.0)
             prog_bar.value = percent
   r.close()
+  print("File download completed.")
 
 
 def display_download_file(): 
@@ -90,6 +123,23 @@ class DerivativeGenerator:
         from pyspark.sql import SQLContext
         self.sqlContext = SQLContext(self.sc)
 
+    def create_csv_with_header(self, headers, datafile, outputfile): 
+      """ Create a version of datafile with the specified headers. 
+
+      Args: 
+        headers: a list of column headers in the desired order
+        datafile: the path of the CSV file, without headers, to add headers to
+        outputfile: the path of the desired output file 
+      """
+      import csv
+      with open(outputfile, "w", newline = "") as csvfile: 
+        writer = csv.writer(csvfile, delimiter=",")
+        writer.writerow(headers)
+        with open(datafile, "r") as datafile: 
+          reader = csv.reader(datafile)
+          for row in reader: 
+            writer.writerow(row)
+
     # a messy first guess at derivative generation
     def generate_derivative(self, source_file, output_folder, file_type="csv", text_filters=0):
         """Create a text derivative file from the specified source file.
@@ -110,7 +160,7 @@ class DerivativeGenerator:
         # import the AUT (needs to be done after the PySpark set-up)
         from aut import WebArchive, remove_html, remove_http_header, extract_boilerplate
         from pyspark.sql.functions import col, desc
-        
+
         # create our WebArchive object from the W/ARC file
         archive = WebArchive(self.sc, self.sqlContext, source_file)
 
@@ -132,18 +182,30 @@ class DerivativeGenerator:
 
         # rename the datafile to have a meaningful title, remove the success file
         success = False
+        # the folder will contain exactly 2 files, a _SUCCESS file and the resulting datafile
         for f in os.scandir(output_folder): 
             if f.path.split("/")[-1] == "_SUCCESS": 
                 # indicate that the derivative was generated successfully
                 success = True
                 # remove the success indicator file
                 os.remove(f.path)
+            # for the datafile
             if f.path.split(".")[-1] == file_type: 
                 source_file_name = source_file.split(".")[0]
                 source_file_name = source_file_name.split("/")[-1]
-                os.rename(f.path, output_folder + source_file_name + "." + file_type)
+                # add the appropriate headers
+                if file_type == "csv": 
+                  headers = []
+                  # for all text_filters between 0 and 2, we'll use the same header
+                  # if adding different derivatives, add the appropriate headers here! 
+                  if text_filters >= 0 and text_filters <= 2: 
+                    headers = ["crawl_date", "domain", "url", "content"]
+                  output_path = output_folder + source_file_name + ".csv"
+                  self.create_csv_with_header(headers, f.path, output_path)
+                  os.remove(f.path)
+                else:   
+                  os.rename(f.path, output_folder + source_file_name + "." + file_type)
         return success
-
 
     def display_derivative_creation_options(self): 
         """ Displays a form to set options for derivative file creation. 
@@ -158,7 +220,7 @@ class DerivativeGenerator:
         passing in the settings specified in the form. 
         """
         # file picker for W/ARC files in the specified folder
-        data_files = [x for x in os.listdir(path) if x.endswith((".warc", ".arc", "warc.gz", ".arc.gz"))]
+        data_files = get_files(path, (".warc", ".arc", "warc.gz", ".arc.gz"))
         file_options = widgets.Dropdown(description="W/ARC file:", options =  data_files)
         out_text = widgets.Text(description="Output folder:", value="output/")
         format_choice = widgets.Dropdown(description="File type:",options=["csv", "parquet"], value="csv")
@@ -199,29 +261,102 @@ class Analyzer:
     def __init__(self): 
         # initialize the data attribute to None -- should possibly be an empty dataframe? consult with appropriate design patterns
         self.data = None
+        self.number_LDA_Topics = None
+
+    def set_data(self, datafile): 
+      """ Sets the data attribute for the Analyzer. 
+
+      Parses columns to appropriate types if applicable. 
+
+      Args: 
+        datafile (str): the path to the datafile to analyze. 
+      """
+      self.data = pd.read_csv(datafile)
+      
+      # if the crawl_date column is included on the frame, make it a date
+      if "crawl_date" in list(self.data): 
+        self.data['crawl_date']= pd.to_datetime(self.data['crawl_date']) #, format='%Y%m%d%H%M%S'
 
     def load_data(self):
         """Load a datafile to work with. 
         """
         # display the options available in the working directory
-        file_options = widgets.Dropdown(description = "Derivative file:", options = [x for x in os.listdir(path) if x.endswith((".csv", ".parquet", ".pqt"))])
+        # Parquet files are not currently supported, if/when they are, add '".parquet", ".pqt"' to the file ending options 
+        label = widgets.Label("Derivative")
+        file_options = widgets.Dropdown(description = "", options = get_files(path, (".csv")))
         button = widgets.Button(description = "Select file")
+        
         def btn_select_file(btn): 
             selected_file = path + "/" + file_options.value
-            self.data = pd.read_csv(selected_file)
-            print(f"File chosen: {selected_file}")
-        button.on_click(btn_select_file)
-        display(file_options)
-        display(button)
+            print("Loading data...")
+            self.set_data(selected_file)
+            print(f"Data loaded from: {selected_file}")
         
+        button.on_click(btn_select_file)
+        display(widgets.HBox([label, file_options, button]))        
+    def date_range_select(self):
+      """ Display a date range selector for valid dates in the data.
+      """
+      from IPython.display import display, Javascript
+      valid_range = self.data.reset_index()['crawl_date'].agg(['min', 'max'])
+      start_label = widgets.Label("Select a start date ")
+      start_picker = widgets.DatePicker(description = "", 
+                                        value = valid_range["min"], 
+                                        disabled = False)
+      start_picker.add_class("start-date")
+      end_label = widgets.Label("Select an end date ")
+      end_picker = widgets.DatePicker(description = "", 
+                                      value = valid_range["max"], 
+                                      disabled = False)
+      end_picker.add_class("end-date")
+
+      js = f"""const query = '.start-date > input:first-of-type';
+           document.querySelector(query).setAttribute('min', '{valid_range['min'].strftime('%Y-%m-%d')}');
+           document.querySelector(query).setAttribute('max', '{valid_range['max'].strftime('%Y-%m-%d')}'); 
+           const q = '.end-date > input:first-of-type';
+           document.querySelector(q).setAttribute('min', '{valid_range['min'].strftime('%Y-%m-%d')}');
+           document.querySelector(q).setAttribute('max', '{valid_range['max'].strftime('%Y-%m-%d')}');"""  
+      script = Javascript(js)
+
+      display(widgets.HBox([start_label, start_picker]))
+      display(widgets.HBox([end_label, end_picker]))
+      display(script)
 
     def display_top_domains(self): 
         """Display the most frequently crawled domains in the dataset.
         """
+        domain_values = self.data["domain"].value_counts();
+        n_domains = len(domain_values)
         def top_domains(n): 
-            print(self.data["domain"].value_counts().head(n))
-        n_slider = widgets.IntSlider()
+            print(domain_values.head(n))
+        n_slider = widgets.IntSlider(value = 10, max = n_domains)
         out = widgets.interactive_output(top_domains, {'n':n_slider})
+        print(f"There are {n_domains} different domains in the dataset. ")
         display(n_slider)
         display(out)
-        
+
+###
+### Topic Modelling Additions
+###
+    def set_LDA_model_topics(self):
+      """ Sets the topic model number of topics for Analyzer Object
+      """
+      t_choice = widgets.BoundedIntText(
+        value = 5,
+        min = 2,
+        max = 25,
+        step = 1,
+        #description = "How many topics for LDA Model?",
+        disabled = False,
+      )
+      t_Button = widgets.Button(description = "Set")
+      
+
+      def btn_set_topics(btn): 
+          self.number_LDA_Topics = t_choice.value
+          print("Topics Set... Ready to prepare model")
+      t_label = widgets.Label("Topics    ")
+      t_Button.on_click(btn_set_topics)
+      display(widgets.HBox([t_label, t_choice,t_Button]))
+
+
